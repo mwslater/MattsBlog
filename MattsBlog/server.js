420e30683cb459028272ec2137b4b04bc74e019b
mwslater <mwslater@kent.edu>
2017-03-28 01:36:06 -0400

    Initial commit of basic blog site

--- MattsBlog/server.js
+++ MattsBlog/server.js
@@ -1,8 +1,102 @@
 'use strict';
-var http = require('http');
-var port = process.env.PORT || 1337;
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/mattblog');
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+var PostSchema = mongoose.Schema({
+    title: { type: String, required: true },
+    body: String,
+    posted: { type: Date, default: Date.now }
+}, { collection: 'post' });
+
+var PostModel = mongoose.model("PostModel", PostSchema);
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.post("/api/blogpost", createPost);
+app.get("/api/blogpost", getAllPosts);
+app.get("/api/blogpost/:id", getPostById);
+app.delete("/api/blogpost/:id", deletePost);
+app.put("/api/blogpost/:id", updatePost);
+
+function updatePost(req, res) {
+    var postId = req.params.id;
+    var post = req.body;
+    PostModel
+        .update({_id: postId}, {
+            title: post.title,
+            body: post.body
+        })
+        .then(
+            function (status) {
+                res.sendStatus(200);
+            },
+            function (err) {
+                res.sendStatus(400);
+            }
+        );
+}
+
+function getPostById(req, res) {
+    var posId = req.params.id;
+    PostModel
+        .findById(postId)
+        .then(
+            function (post) {
+                res.json(post);
+            },
+            function (err) {
+                res.sendStatus(400);
+            }
+        );
+}
+
+function deletePost(req, res) {
+    var postId = req.params.id;
+    PostModel
+        .remove({_id: postId})
+        .then(
+            function (status) {
+                res.sendStatus(200);
+            },
+            function () {
+                res.sendStatus(400);
+            }
+        );
+}
+
+function getAllPosts(req, res) {
+    PostModel
+        .find()
+        .then(
+            function (posts) {
+                res.json(posts);
+            },
+            function (err) {
+                res.sendStatus(400);
+            }
+        );
+}
+
+function createPost(req, res) {
+    var post = req.body;
+    console.log(post);
+    PostModel
+        .create(post)
+        .then(
+            function (postObj) {
+                res.json(200);
+             },
+            function (error) {
+                res.sendStatus(400);
+            }
+
+        );
+    
+}
+
+app.listen(3000);
